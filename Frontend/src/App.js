c99d58504957c134847bc5e28c10529bd41ed97c
matuteduro <matias_duro@hotmail.com>
2022-06-23 18:50:09 -0300

    products

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,6 +6,7 @@ import WebFont from 'webfontloader';
 import Footer from "./components/layout/Footer/Footer"
 import Home from "./components/Home/Home";
 import ProductDetails from "./components/Product/ProductDetails"
+import Products from "./components/Product/Products"
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/product/:id" element={<ProductDetails/>}/>
+        <Route exact path="/products" element={<Products/>}/>
       </Routes>
     <Footer/>
     </Router>
