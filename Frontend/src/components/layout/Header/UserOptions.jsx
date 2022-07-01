d2284acabe0429168b050f4e9b23f6fe1634716d
matuteduro <matias_duro@hotmail.com>
2022-07-01 20:27:29 -0300

    cart fix

--- Frontend/src/components/layout/Header/UserOptions.jsx
+++ Frontend/src/components/layout/Header/UserOptions.jsx
@@ -6,13 +6,14 @@ import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
 import PersonIcon from '@mui/icons-material/Person';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import ListAltIcon from '@mui/icons-material/ListAlt';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
 import { useNavigate } from 'react-router-dom';
 import {useAlert} from "@blaumaus/react-alert"
 import {logout} from "../../../actions/userAction"
-import {useDispatch} from "react-redux"
+import {useDispatch, useSelector} from "react-redux"
 
 const UserOptions = ({user}) => {
-    
+    const {cartItems} = useSelector((state) => state.cart)
     const [open, setOpen] = useState(false)
     const navigate = useNavigate();
     const alert = useAlert();
@@ -21,6 +22,7 @@ const UserOptions = ({user}) => {
     const options = [
         {icon: <ListAltIcon/>, name: "Orders", func: orders },
         {icon: <PersonIcon/>, name: "Profile", func: account },
+        {icon: <ShoppingCartIcon style={{color:cartItems.length>0? "green" : "unset"}}/>, name: `Cart(${cartItems.length})`, func:cart},
         {icon: <ExitToAppIcon/>, name: "Logout", func: logoutUser },
    ];
 
@@ -40,6 +42,9 @@ const UserOptions = ({user}) => {
    }
    function account(){
     navigate("/account");
+   }
+   function cart(){
+    navigate("/cart");
    }
    function logoutUser(){
     dispatch(logout());
@@ -61,7 +66,7 @@ return(
         {options.map((item) =>(
         <SpeedDialAction 
         key={item.name}
-        icon={item.icon} tooltipTitle={item.name} onClick={item.func}/>
+        icon={item.icon} tooltipTitle={item.name} onClick={item.func} tooltipOpen={window.innerWidth<=600?true:false}/>
         ))}
     </SpeedDial>
   </Fragment>
