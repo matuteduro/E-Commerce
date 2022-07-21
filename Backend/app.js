e374a66cc74985dd8efb972f514e19d591508b8f
matuteduro <matias_duro@hotmail.com>
2022-07-21 20:26:37 -0300

    vercell

--- Backend/app.js
+++ Backend/app.js
@@ -23,7 +23,6 @@ const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
 const payment = require("./routes/paymentRoute");
 
-
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
